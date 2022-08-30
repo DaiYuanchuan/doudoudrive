308509eadabf5c91e10849af0dff936c85880d0a
Dan <doudou@doudoudrive.com>
2022-08-31 03:47:35 +0800

    fix(v0.1.0): yml中增加xxl-job配置，重命名文件定时任务处理程序

--- doudoudrive-task/src/main/java/com/doudoudrive/task/job/FileJobHandler.java
+++ doudoudrive-task/src/main/java/com/doudoudrive/task/job/FileJobHandler.java
@@ -1,27 +1,29 @@
 package com.doudoudrive.task.job;
 
+import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
- * <p>删除云端文件的定时任务（Bean模式）</p>
+ * <p>文件操作相关定时任务处理程序（Bean模式）</p>
  * <p>2022-08-30 19:30</p>
  *
  * @author Dan
  **/
 @Slf4j
 @Component
-public class DeleteFileJob {
+public class FileJobHandler {
 
     /**
      * 查找 oss_file 表中存在，但是 disk_file 表中不存在的文件信息
      * 配置定时任务 ，每月1号凌晨1点执行(cron = 0 0 1 1 * ?)删除云端文件
      */
     @XxlJob(value = "deleteFileJobHandler")
-    public void deleteFileJobHandler() {
+    public ReturnT<String> deleteFileJobHandler() {
         log.info("删除云端文件的定时任务（Bean模式）");
         XxlJobHelper.log("java, Hello World~~~ 删除云端文件的定时任务（Bean模式）--");
+        return ReturnT.SUCCESS;
     }
 }
