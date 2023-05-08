4bab09920ed21d82bd9438d0201a0045b51a507f
Dan <doudou@doudoudrive.com>
2023-05-09 00:22:22 +0800

    fix(1.0.0): 修复404异常，增加info检查配置

--- doudoudrive-common/src/main/java/com/doudoudrive/common/config/ViewConfig.java
+++ doudoudrive-common/src/main/java/com/doudoudrive/common/config/ViewConfig.java
@@ -8,9 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * <p>页面视图配置</p>
  * <p>2022-03-21 15:18</p>
@@ -23,9 +20,19 @@ public class ViewConfig {
 
     @SneakyThrows
     @RequestMapping(value = "/404", produces = ConstantConfig.HttpRequest.CONTENT_TYPE_JSON_UTF8)
-    public Result<String> jumpNotFound(HttpServletRequest request, HttpServletResponse response) {
-        request.setCharacterEncoding(ConstantConfig.HttpRequest.UTF8);
-        response.setContentType(ConstantConfig.HttpRequest.CONTENT_TYPE_JSON_UTF8);
+    public Result<String> jumpNotFound() {
         return Result.build(StatusCodeEnum.NOT_FOUND);
+    }
+
+    @SneakyThrows
+    @RequestMapping(value = "/500", produces = ConstantConfig.HttpRequest.CONTENT_TYPE_JSON_UTF8)
+    public Result<String> jumpError() {
+        return Result.build(StatusCodeEnum.SYSTEM_ERROR);
+    }
+
+    @SneakyThrows
+    @RequestMapping(value = "/info", produces = ConstantConfig.HttpRequest.CONTENT_TYPE_JSON_UTF8)
+    public Result<String> jumpInfo() {
+        return Result.build(StatusCodeEnum.SUCCESS);
     }
 }
